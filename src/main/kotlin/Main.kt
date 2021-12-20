06c38b421f36dcd6f2a17953ceeca157295b2eb9
Reemaldossari <92865481+Reemaldossari@users.noreply.github.com>
2021-12-20 22:54:43 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+
+fun main () {
+
+    var name: String? = null
+
+    var num1: String? = null
+    num1?.length
+
+
+    try {
+
+        var num2: String = ""
+
+        println(num2)
+
+    } catch (a: Exception) {
+
+        println("error")
+    }
+}
+
+
+
+
